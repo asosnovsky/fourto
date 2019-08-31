aa50cb0b46dcf88c7a4f31b59b6f7abf6fe39fde
Ariel Sosnovsky <ariel@sosnovsky.ca>
2019-08-31 19:08:23 -0400

    cleaned up more styles

---
+++ src/config/_secrets.ts
@@ -0,0 +1,8 @@
+export default {
+    "firebase": {
+        apiKey: "AIzaSyBGHcCl7P1QVpPqYZZLjLUiToMEYt2qkYU",
+        projectId: "fourto-d7cba",
+        appId: "1:417126755546:web:d7498263615af2cb",
+        databaseURL: "https://fourto-d7cba.firebaseio.com"
+    }
+}
