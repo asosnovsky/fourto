f5e9f7ec86976a4da693f1d4864b28c5637e8c5b
Ariel Sosnovsky <ariel@sosnovsky.ca>
2019-09-04 13:49:52 -0400

    fixed home-btn css and replace the arrow with an icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,9 @@ export default function Navbar (props: Props) {
                 bannerState.confirm("Going back to main page, this means that your game is over.", () => {
                     history.push("/")
                 })
-            }}>&larr;</button>
+            }} className="btn-home">
+                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAQAAAC0NkA6AAAAAmJLR0QA/4ePzL8AAAE5SURBVFjD7dQ9S8NAHMfxb6Uogo+DDnXJVhdxclNERPAdiEtxcnbrW8jq6OokCCKIoxbqw1AKTi7i4mJR6VCQDlr6d0iuBNvo9bxDkft9txD4DLkL/MEFlCgRuCRWeEIQ6qy7ATIUaSFxbUIGbBNjHHUA1SmTNol57rsIQXhgIX4j+dRoBZo9CUFoUgBghzdzZIjdVEC1zzCwyKMZMsP1t4QgVAmAKc76R5apaRGC8MIakCWkbXpgdWpRJANs6BKjHPYFqE6Y0CVmuTUiBOGOOR1ik1djIjrUW18DWcIfAao9BtOIac6tEIJwSa4XsdS5THZ6ZvUzsZ34LdjqPT7UAIxwYB1QHTMeIRWEhhOigVCJkCtuyDtB8lQpJ7+KC6RrHvHIv0bS5hGPeMQjHvHILyAX1pEyfib7AN2GgOq5GZnpAAAAAElFTkSuQmCC"/>
+            </button>
             <button className="blue" onClick={() => props.onUndo()}>Undo Last Move</button>       
             <button className="red" onClick={() => {
                 bannerState.confirm("Restarting the game.", () => {
